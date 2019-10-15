3d4fd20fdcecf5e562f3e26a236118fccf4c3815
gy991007 <54530838+gy991007@users.noreply.github.com>
2019-10-15 23:41:19 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "varmap.h"
+int main(){
+	variable a=variable(1.0);
+	variable b=variable(2.2);
+	variable c=variable("string");
+	//vector<variable> v = {a,b,c};
+	//variable l= variable(v);
+	a.print();
+	b.print();
+	c.print();
+	//l.print();
+	//vector<variable> vp = vector<variable>();
+	//variable *pln=new variable(vp);
+	//v[0]=*pl;
+	//v[1]=*pln;
+	//v[2]=*pc;
+	//variable *pll=new variable(v);
+	//pll->print();
+	//pln->print();
+	//variable *pn=new variable();
+	//pn->print();
+	//varmap vm;
+	//vm.crevar("a", a);
+	//vm.crevar("b", b);
+	//vm.crevar("c", a);
+	//vm.print("a");
+	//return 0;
+	
+}
